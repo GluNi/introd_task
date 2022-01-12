b2cbbab3afff6707fd3d4839154a2c89f7bf8280
GluNi <nikolaikutafin@yandex.ru>
2022-01-12 10:58:12 +0300

    The Last2

--- MyTable1/static/scripts/check23.js
+++ MyTable1/static/scripts/check23.js
@@ -16,75 +16,37 @@ function conditions(opp) {
 	supconditions(opp);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-
-	//document.querySelector("#calculate").disabled = false;
-
-	/*document.querySelector('#log_x').addEventListener('change', function() {
+document.addEventListener('DOMContentLoaded', () => {	
+	/* document.querySelector('#log_x').addEventListener('change', function() {
 		if (document.querySelector('#log_x').checked) {
 			
-			console.log("eeee")	
+			console.log("ee");	
 		} else {
-			console.log("aaaa")
+			console.log("aa");
 		}
 
-	});
+	});	*/
 
-	document.querySelector('#log_y').addEventListener('change', function() {
-		if (document.querySelector('#log_y').checked) {
-			
-			console.log("eeee")	
-		} else {
-			console.log("aaaa")
-		}
+	d1 = document.querySelector('.registration');
+	d2 = document.querySelector('.LastActivity');
+	const now = new Date();	
+	if (!(Date.parse(d1.value)<=Date.parse(d2.value) && Date.parse(d2.value)<=Date.parse(now))) {
+		document.querySelector("#save").disabled = true;
+	}	
 
-	});*/
-	
-	// Set limitation up to current date in a calendar
 	for (let elem of document.querySelectorAll('input[type=date]')) {
 		elem.setAttribute("max", new Date().toISOString().split('T')[0]);
-
-		
-		supconditions(elem);
-	}
-	
-	document.querySelector('.LastActivity').addEventListener('change', function() {
-		const time = new Date();
-		if (document.querySelector('.LastActivity').value && document.querySelector('.registration').value) {
-			if (Date.parse(document.querySelector('.LastActivity')).value <= Date.parse(time)) {
-				if (Date.parse(document.querySelector('.registration')).value <= Date.parse(document.querySelector('.LastActivity')).value) {
-					document.querySelector('.registration').style.border= "solid green 2px";
-					document.querySelector('.LastActivity').style.border= "solid green 2px";
-					document.querySelector('.registration').setAttribute("max", document.querySelector('.LastActivity')); 
-				} else { document.querySelector('.registration').style.border= "solid red 2px";
-					document.querySelector('.LastActivity').style.border= "solid green 2px";
-				}
-			} 
-		else { 
-			if (document.querySelector('.LastActivity').value) {
-				if (Date.parse(document.querySelector('.LastActivity').value) <= Date.parse(time)) {
-					document.querySelector('.LastActivity').style.border= "solid green 2px";
-					document.querySelector('.registration').setAttribute("max", document.querySelector('.LastActivity')); 
-
-				} else {
-					document.querySelector('.LastActivity').style.border= "solid red 2px";
-				}
-			} else {
-				if (Date.parse(document.querySelector('.registration').value) <= Date.parse(time)) {
-					document.querySelector('.registration').style.border= "solid green 2px";
-				} else {
-					document.querySelector('.registration').style.border= "solid red 2px";
-				}
-
+		elem.addEventListener('change', function() {
+			if (Date.parse(d1.value)<=Date.parse(d2.value) && Date.parse(d2.value)<=Date.parse(now)) {				
+				document.querySelector("#save").disabled = false;
+			} else {				
+				document.querySelector("#save").disabled = true;
 			}
-
-			}
-	}
- });		
+		});
+	}	
 });
 
 
 
 
-
-
+	
